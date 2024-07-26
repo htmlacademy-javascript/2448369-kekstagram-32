356bba02969f8cf7b4b06a24150f53adf75090ff
Anna Baidikova <annabaidikova92@gmail.com>
2024-07-26 16:11:57 +0200

    module8-task1

--- js/thumbnailGenerator.js
+++ js/thumbnailGenerator.js
@@ -1,18 +1,19 @@
 const thumbnailTemplate = document.querySelector('#picture').content.querySelector('.picture');
-const container = document.querySelector('.pictures');
+// const container = document.querySelector('.pictures'); перенесли в gallery.js
 
-const createThumbnail = ({url, description, likes, comments}) => {
+const createThumbnail = ({url, description, likes, comments, id }) => {
   const thumbnail = thumbnailTemplate.cloneNode(true);
   thumbnail.querySelector('.picture__img').src = url;
   thumbnail.querySelector('.picture__img').alt = description;
   thumbnail.querySelector('.picture__likes').textContent = likes;
   thumbnail.querySelector('.picture__comments').textContent = comments.length;
+  thumbnail.dataset.thumbnailId = id;
 
   return thumbnail;
 
 };
 
-const thumbnailGenerator = (pictures) => {
+const renderThumbnails = (pictures, container) => {
   const fragment = document.createDocumentFragment();
 
   pictures.forEach((picture) => {
@@ -23,4 +24,4 @@ const thumbnailGenerator = (pictures) => {
   container.append(fragment);
 };
 
-export {thumbnailGenerator};
+export {renderThumbnails};
