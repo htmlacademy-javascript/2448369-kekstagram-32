142f6ca55d9bb6e536422d16cced4a61ec8d47f0
Anna Baidikova <annabaidikova92@gmail.com>
2024-07-30 21:57:19 +0200

    pristine

--- js/form_utils.js
+++ js/form_utils.js
@@ -22,16 +22,57 @@ const closeUploadInput = () => {
 };
 
 function onDocumentKeydown(evt) {
-  if (isEscapeKey(evt)) {
+  if (isEscapeKey(evt) && !document.activeElement.classList.contains('text__hashtags')) {
     evt.preventDefault();
     closeUploadInput();
   }
 }
 
-function resetForm () {
+function resetForm() {
   uploadImg.value = ''; // Сбрасываем значение поля выбора файла
   document.querySelector('#upload-select-image').reset(); // Сбрасываем все значения полей формы
 }
 
 uploadImgCancel.addEventListener('click', closeUploadInput);
 
+const pristine = new Pristine(form, {
+  classTo: 'img-upload__field-wrapper',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextClass: 'img-upload__field-wrapper__error',
+});
+
+pristine.addValidator(hashtagField, (value) => {
+  if (!value) {
+    return true; // Хэштеги необязательны
+  }
+
+  const hashtags = value.trim().split(/\s+/);
+
+  if (hashtags.length > 5) {
+    return false; // нельзя указать больше пяти хэштегов
+  }
+
+  const hashtagPattern = /^#[a-za-яё0-9]{1,19}$/i;
+  const uniqueHashtags = new Set();
+
+  for (let hashtag of hashtags) {
+    if (!hashtagPattern.test(hashtag)) {
+      return false; // валидация каждого хэштега по условиям
+    }
+    if (uniqueHashtags.has(hashtag.toLowerCase())) {
+      return false; // один и тот же хэштег не может быть использован дважды
+    }
+    uniqueHashtags.add(hashtag.toLowerCase());
+  }
+
+  return true;
+}, 'Некорректный хэштег. Хэштег должен начинаться с символа # и содержать только буквы и цифры, максимум 20 символов, и не может повторяться.');
+
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  const isValid = pristine.validate();
+  if (isValid) {
+    // Отправка формы
+    form.submit();
+  }
+});
